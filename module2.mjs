efca9afcf96328f668516e7a9b070b042a3eeb81
Radhika Agarwal <radhikaag393@gmail.com>
2024-09-11 12:36:05 +0530

    functionality add

---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a = "Radhika";
+const b = "Rohan";
+const c = "Aakash";
+const d = "Priyanka";
+
+export default b;  // by default can be named as anything eg. dza
+export { a };  // curly braces used to export other things than default.
+export { c };
+export { d };
